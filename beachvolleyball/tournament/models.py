a1c8b39081bf8a25bfbeff1cd5502c83e83b185e
jinjan <jindrizka.alejandra@gmail.com>
2019-03-28 11:52:08 -0600

    Modified templates to display posts from database.

--- beachvolleyball/tournament/models.py
+++ beachvolleyball/tournament/models.py
@@ -7,5 +7,8 @@ class Tournament(models.Model):
     date = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
 
+    def publish(self):
+        self.save()
+
     def __str__(self):
         return self.name
--- beachvolleyball/tournament/views.py
+++ beachvolleyball/tournament/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Tournament
 
 def tournament_list(request):
-    return render(request, 'tournament/tournament_list.html', {})
+    tours = Tournament.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request, 'tournament/tournament_list.html', {'tours':tours})
