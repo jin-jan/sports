10a9ecd960ea284afaf4140aaac7f2e1f7c8659d
jinjan <jindrizka.alejandra@gmail.com>
2019-04-03 17:25:58 -0600

    Docker setup

--- build_docker_env.sh
+++ build_docker_env.sh
@@ -5,7 +5,7 @@ PG_PASSWORD=`head -c 18 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | head -c 12`
 PG_SERVICE_NAME=postgres
 PG_USER=webdevjinjan9_user
 
-echo "POSTGRES_DB=$PG_DB"
+echo "POSTGRES_DB=$PG_DB
 POSTGRES_PASSWORD=$PG_PASSWORD
 POSTGRES_USER=$PG_USER
 DATABASE_URL=postgres:://$PG_USER:$PG_PASSWORD@$PG_SERVICE_NAME:5432/$PG_DB
