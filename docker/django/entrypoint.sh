fbfbd8a971760b6bbe5a003473d3bbc10726adc8
jinjan <jindrizka.alejandra@gmail.com>
2019-04-03 17:40:27 -0600

    Fixed identation entrypoint

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -6,21 +6,21 @@ then
 fi
 
 postgres_ready() {
-  python3.7 << END
-  from sys import exit
-  from psycopg2 import connect, OperationalError
-  try:
-      connect(
-          dbname="$POSTGRES_DB",
-          user="$POSTGRES_USER",
-          password="$POSTGRES_PASSWORD",
-          host="postgres",
-      )
-  except OperationalError as error:
-      print(error)
-      exit(-1)
-  exit(0)
-  END
+python3.7 << END
+from sys import exit
+from psycopg2 import connect, OperationalError
+try:
+    connect(
+        dbname="$POSTGRES_DB",
+        user="$POSTGRES_USER",
+        password="$POSTGRES_PASSWORD",
+        host="postgres",
+    )
+except OperationalError as error:
+    print(error)
+    exit(-1)
+exit(0)
+END
 }
 
 until postgres_ready; do
